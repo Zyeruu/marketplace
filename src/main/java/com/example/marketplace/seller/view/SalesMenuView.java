36413957a9fad1325a5a1073ef1fbc53195aacf8
Zyeruu <104399169+Zyeruu@users.noreply.github.com>
2026-05-31 19:28:32 -0300

    refactor: centralize printing logic into 'printOrder' method to eliminate duplication

--- src/main/java/com/example/marketplace/seller/view/SalesMenuView.java
+++ src/main/java/com/example/marketplace/seller/view/SalesMenuView.java
@@ -11,43 +11,58 @@ public final class SalesMenuView {
 
     private SalesMenuController controller;
 
-    public void printSalesMenu(String email) {
+    public void printSalesMenu(String email, String cnpj) {
 
-        List<TaxReceipt> taxReceiptList = controller.searchUser(email);
+        List<TaxReceipt> taxReceiptList = controller.findByEmailAndCnpj(email, cnpj);
 
-        if (!taxReceiptList.isEmpty()) {
+        if (taxReceiptList != null)
+            if (!taxReceiptList.isEmpty()) {
 
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+                System.out.println("\n-------- SALES MENU --------\n");
 
-            for (TaxReceipt taxReceipt : taxReceiptList) {
+                for (TaxReceipt taxReceipt : taxReceiptList)
+                    printOrder(taxReceipt);
 
-                System.out.println("\n-------- TAX RECEIPT --------\n");
-                System.out.println("Order ID: " + taxReceipt.getOrderId());
-                System.out.println("Buyer name: " + taxReceipt.getBuyerName());
-                System.out.println("Seller name: " + taxReceipt.getSellerName());
-
-                String formattedDate = taxReceipt.getDateTime().format(formatter);
-                System.out.println("Date: " + formattedDate);
-
-                System.out.printf("Total cost: R$%.2f\n", taxReceipt.getTotalCost());
-
-                if (taxReceipt.getShipping() == 0)
-                    System.out.println("Shipping: Free");
-                else
-                    System.out.printf("Shipping: R$%.2f\n", taxReceipt.getShipping());
-
-                for (OrderedItem orderedItem : taxReceipt.getOrderedItemsList()) {
-
-                    System.out.println("\n-------- ITEMS --------\n");
-                    System.out.println("Name: " + orderedItem.getName());
-                    System.out.println("ID: " + orderedItem.getId());
-                    System.out.println("Type: " + orderedItem.getType().name());
-                    System.out.println("Quantity: " + orderedItem.getQuantity());
-                    System.out.printf("Unit price: R$%.2f\n", orderedItem.getUnitPrice());
-                    System.out.printf("Total cost: R$%.2f\n", orderedItem.getTotalCost());
-                }
+                System.out.println("Total items sold: " + taxReceiptList.size());
             }
-            System.out.println("Total items sold: " + taxReceiptList.size());
+    }
+
+    public void printTaxReceipt(String email, String cnpj, int orderId) {
+
+        TaxReceipt taxReceipt = controller.findByEmailAndCnpjAndOrderId(email, cnpj, orderId);
+
+        if (taxReceipt != null)
+            printOrder(taxReceipt);
+    }
+
+    public void printOrder(TaxReceipt taxReceipt) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        System.out.println("\n-------- TAX RECEIPT --------\n");
+        System.out.println("Order ID: " + taxReceipt.getOrderId());
+        System.out.println("Buyer name: " + taxReceipt.getBuyerName());
+        System.out.println("Seller name: " + taxReceipt.getSellerName());
+
+        String formattedDate = taxReceipt.getDateTime().format(formatter);
+        System.out.println("Date: " + formattedDate);
+
+        System.out.printf("Total cost: R$%.2f\n", taxReceipt.getTotalCost());
+
+        if (taxReceipt.getShipping() == 0)
+            System.out.println("Shipping: Free");
+        else
+            System.out.printf("Shipping: R$%.2f\n", taxReceipt.getShipping());
+
+        for (OrderedItem orderedItem : taxReceipt.getOrderedItemsList()) {
+
+            System.out.println("\n-------- ITEMS --------\n");
+            System.out.println("Name: " + orderedItem.getName());
+            System.out.println("ID: " + orderedItem.getId());
+            System.out.println("Type: " + orderedItem.getType().name());
+            System.out.println("Quantity: " + orderedItem.getQuantity());
+            System.out.printf("Unit price: R$%.2f\n", orderedItem.getUnitPrice());
+            System.out.printf("Total cost: R$%.2f\n", orderedItem.getTotalCost());
         }
     }
 }
