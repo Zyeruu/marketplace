aaaf3de400ede648ff400d1db8cfd85df03368f8
Zyeruu <104399169+Zyeruu@users.noreply.github.com>
2026-06-02 17:35:03 -0300

    feat: add AlreadyExistsException to exceptions package

---
+++ src/main/java/com/example/marketplace/exceptions/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package main.java.com.example.marketplace.exceptions;
+
+public class AlreadyExistsException extends RuntimeException {
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
